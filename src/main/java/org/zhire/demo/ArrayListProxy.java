cdf64656efecdc7d9b43a9dc04e1c13e6b7fbf3b
chenqi <chenqi@live.hk>
2020-05-11 11:12:17 +0800

    filter过滤 动态代理

---
+++ src/main/java/org/zhire/demo/ArrayListProxy.java
@@ -0,0 +1,28 @@
+package org.zhire.demo;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 动态代理
+ *
+ * @Author: chenqi
+ * @Date: 2020.5.7 16:29
+ */
+public class ArrayListProxy {
+    public static void main(String[] args) {
+        List list = new ArrayList();
+        List proxyInstance = (List) Proxy.newProxyInstance(list.getClass().getClassLoader(), list.getClass().getInterfaces(),
+                new InvocationHandler() {
+                    @Override
+                    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+                        return method.invoke(list, args);
+                    }
+                });
+        proxyInstance.add("hello");
+        System.out.println(list);
+    }
+}
--- src/main/java/org/zhire/demo/JavaStreamTest.java
+++ src/main/java/org/zhire/demo/JavaStreamTest.java
@@ -216,4 +216,15 @@ public class JavaStreamTest {
         System.out.println(list);
 
     }
+
+    /**
+     * filter过滤
+     */
+    @Test
+    public void testFiltef(){
+        //创建一个字符串列表，每个字符串长度大于2
+        List<String>  costBeforeTax = Arrays.asList("abc","bcd","defg","jk");
+        List<String> filtered = costBeforeTax.stream().filter(x -> x.length() > 2).collect(Collectors.toList());
+        System.out.printf("Original List : %s, filtered list : %s %n", costBeforeTax, filtered);
+    }
 }
