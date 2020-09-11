f8bc48f910a39161144e3ab37faee5b0a9d7db3b
chenqi <chenqi@live.hk>
2020-09-11 14:10:11 +0800

    控制器的通知

--- src/main/java/org/zhire/controller/MyControllerAdvice.java
+++ src/main/java/org/zhire/controller/MyControllerAdvice.java
@@ -1,5 +1,7 @@
 package org.zhire.controller;
 
+import com.google.common.base.Throwables;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.WebDataBinder;
@@ -7,12 +9,20 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.InitBinder;
 
+import javax.servlet.http.HttpServletRequest;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
+import java.util.Enumeration;
 
 /**
  * 控制器的通知
  */
+@Slf4j
 @ControllerAdvice(
         // 指定拦截的包
         basePackages = {"org.zhire.controller.*"},
@@ -33,9 +43,54 @@ public class MyControllerAdvice {
 
     // 异常处理，使得被拦截的控制器方法发生异常时，都能用相同的视图响应
     @ExceptionHandler(value = Exception.class)
-    public Object exception(Exception ex) {
-        System.out.println("异常：" + ex.getMessage());
-        // 返回异常视图
+    public Object exception(HttpServletRequest request, Exception ex) {
+        String content = ExceptionUtil.print(request, ex);
+        log.error("异常：{}", content);
         return ex;
+    }
+
+    public static class ExceptionUtil {
+        private final static char COLON = ':';
+        private final static char NEWLINES = '\n';
+        private static String HOSTNAME;
+
+        static {
+            try {
+                HOSTNAME = InetAddress.getLocalHost().getHostAddress();
+            } catch (UnknownHostException e) {
+                HOSTNAME = "UnknownHost";
+            }
+        }
+
+        @SuppressWarnings({"unchecked", "rawtypes"})
+        static String print(HttpServletRequest request, Throwable e) {
+            return getString(request, e, HOSTNAME, getRequestTime());
+        }
+
+        static String getString(HttpServletRequest request, Throwable e, String hostname, String requestTime) {
+            StringBuffer sb = new StringBuffer();
+            sb.append(ExceptionUtil.NEWLINES).append(ExceptionUtil.NEWLINES);
+            sb.append("HostName").append(ExceptionUtil.COLON).append(hostname).append(ExceptionUtil.NEWLINES);
+            sb.append("ErrorTime").append(ExceptionUtil.COLON).append(requestTime).append(ExceptionUtil.NEWLINES);
+            sb.append("Error").append(ExceptionUtil.COLON).append(Throwables.getRootCause(e).getMessage()).append(ExceptionUtil.NEWLINES);
+            sb.append("Resources").append(ExceptionUtil.COLON).append(request.getMethod()).append(" ").append(request.getRequestURI()).append(ExceptionUtil.NEWLINES);
+            sb.append("RequestParameters {").append(ExceptionUtil.NEWLINES);
+            request.getParameterMap().keySet().forEach(k -> sb.append("\t").append(k).append(ExceptionUtil.COLON).append(request.getParameter((String) k)).append(ExceptionUtil.NEWLINES));
+            sb.append("}").append(ExceptionUtil.NEWLINES);
+            sb.append("RequestHeaders {").append(ExceptionUtil.NEWLINES);
+            Enumeration headers = request.getHeaderNames();
+            while (headers.hasMoreElements()) {
+                String key = headers.nextElement().toString();
+                sb.append("\t").append(key).append(":").append(request.getHeader(key)).append(ExceptionUtil.NEWLINES);
+            }
+            sb.append("}").append(ExceptionUtil.NEWLINES);
+            sb.append(ExceptionUtil.NEWLINES);
+            sb.append(Throwables.getStackTraceAsString(e));
+            return sb.toString();
+        }
+
+        private static String getRequestTime() {
+            return LocalDateTime.now(ZoneOffset.systemDefault()).format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+        }
     }
 }
