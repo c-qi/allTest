2d0a151a890d44329bd853be92ac2a61bc284d64
chenqi <chenqi@live.hk>
2020-11-27 18:45:58 +0800

    剑指 Offer 44. 数字序列中某一位的数字

---
+++ src/main/java/org/zhire/suanfa/practise/Jz44.java
@@ -0,0 +1,33 @@
+package org.zhire.suanfa.practise;
+
+import org.junit.Test;
+
+/**
+ * 剑指 Offer 44. 数字序列中某一位的数字
+ * 数字以0123456789101112131415…的格式序列化到一个字符序列中。在这个序列中，第5位（从下标0开始计数）是5，第13位是1，第19位是4，等等。
+ * 请写一个函数，求任意第n位对应的数字。
+ * 示例 1：
+ * 输出：3
+ * 示例 2：
+ * 输入：n = 11
+ * 输出：0
+ * 限制：
+ * 0 <= n < 2^31
+ *
+ * @Date 2020/11/24 14:38
+ * @Author by chenqi
+ */
+public class Jz44 {
+    public int findNthDigit(int n) {
+        String v = Math.pow(2, 30) + "";
+        System.out.println(Integer.parseInt(v + ""));
+        return 0;
+
+    }
+
+    @Test
+    public void t() {
+        findNthDigit(0);
+
+    }
+}
