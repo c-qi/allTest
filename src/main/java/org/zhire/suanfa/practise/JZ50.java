a109898f0b809754c4763b65d41e844a314584b9
chenqi <chenqi@live.hk>
2021-01-09 18:59:17 +0800

    * 剑指 Offer 50. 第一个只出现一次的字符

---
+++ src/main/java/org/zhire/suanfa/practise/JZ50.java
@@ -0,0 +1,51 @@
+package org.zhire.suanfa.practise;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ * 剑指 Offer 50. 第一个只出现一次的字符
+ * 在字符串 s 中找出第一个只出现一次的字符。如果没有，返回一个单空格。 s 只包含小写字母。
+ * <p>
+ * 示例:
+ * <p>
+ * s = "abaccdeff"
+ * 返回 "b"
+ * <p>
+ * s = ""
+ * 返回 " "
+ *
+ * @Date 2021/1/9 18:31
+ * @Author by chenqi
+ */
+public class JZ50 {
+    public char firstUniqChar(String s) {
+        if (null == s || "".equals(s)) return ' ';
+        if (s.length() == 1) return s.charAt(0);
+        char[] array = s.toCharArray();
+        HashMap<Character, Integer> hashMap = new HashMap<>();
+        for (int i = 0; i < array.length; i++) {
+            if (null == hashMap.get(array[i]))
+                hashMap.put(array[i], 1);
+            else hashMap.put(array[i], hashMap.get(array[i]) + 1);
+        }
+        ArrayList<Object> list = new ArrayList<>();
+        hashMap.forEach((k, v) -> {
+            if (v == 1) {
+                System.out.println(k);
+                list.add(k);
+            }
+        });
+        System.out.println(list.get(0));
+        return ' ';
+    }
+
+    @Test
+    public void t() {
+        System.out.println(firstUniqChar("leetcode"));
+    }
+
+
+}
