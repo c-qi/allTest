6bb9627384b3f9b3a618b8b291ee79e84e9244bf
chenqi <chenqi@live.hk>
2021-01-19 15:56:26 +0800

    * 剑指 Offer 55 - I. 二叉树的深度（递归求左右子树的深度，他们深度的最大值加1）

---
+++ src/main/java/org/zhire/suanfa/practise/JZ55.java
@@ -0,0 +1,57 @@
+package org.zhire.suanfa.practise;
+
+import lombok.Data;
+import org.junit.Test;
+
+/**
+ * 剑指 Offer 55 - I. 二叉树的深度
+ * 输入一棵二叉树的根节点，求该树的深度。从根节点到叶节点依次经过的节点（含根、叶节点）形成树的一条路径，最长路径的长度为树的深度。
+ * <p>
+ * 例如：
+ * <p>
+ * 给定二叉树 [3,9,20,null,null,15,7]，
+ * <p>
+ * 3
+ * / \
+ * 9  20
+ * /  \
+ * 15   7
+ * 返回它的最大深度 3 。
+ *
+ * @Date 2021/1/19 15:44
+ * @Author by chenqi
+ */
+public class JZ55 {
+    @Data
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    /**
+     * 递归求左右子树的深度，他们深度的最大值加1就是整个树的深度
+     */
+    public int maxDepth(TreeNode root) {
+        if (root == null) return 0;
+        int l = maxDepth(root.left);
+        int r = maxDepth(root.right);
+        return l > r ? l + 1 : r + 1;
+
+    }
+
+    @Test
+    public void t() {
+        TreeNode treeNode = new TreeNode(3);
+        treeNode.left = new TreeNode(1);
+        treeNode.right = new TreeNode(4);
+        treeNode.left.right = new TreeNode(2);
+        System.out.println(maxDepth(treeNode));
+    }
+
+
+}
